8692fa7126f31729beb796fb2342fd822a9f5282
matveygrevtsov <43029770+matveygrevtsov@users.noreply.github.com>
2024-03-25 11:46:35 +0300

    add default routes (#3)
    Co-authored-by: Grevtsov Matvey <matvey.grevtsov@ax-team.corp>

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -13,10 +13,19 @@ const Header = ({ items, onLogout }: HeaderProps) => {
   const location = useLocation();
   const navigate = useNavigate();
 
-  const selectedKeys = useMemo<string[]>(
-    () => (location?.pathname ? [location.pathname] : []),
-    [location?.pathname]
-  );
+  const selectedKeys = useMemo<string[]>(() => {
+    const allKeys = items
+      .map((item) => (item?.key ? `${item.key}` : ""))
+      .filter((key) => key);
+
+    if (allKeys.length === 0) return [];
+
+    if (allKeys.includes(location?.pathname)) {
+      return [location.pathname];
+    }
+
+    return [allKeys[0]];
+  }, [location?.pathname, items]);
 
   const handleMenuClick = ({ key }: { key: string }) => {
     navigate(key);
